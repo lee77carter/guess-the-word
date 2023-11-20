92cb9d2ce76aaf8866e1e8ea57a47a5e305db4ed
lee_c <gitleelee@gmail.com>
2023-11-20 12:49:20 -0800

    Added placeholder function and event listener.

--- js/script.js
+++ js/script.js
@@ -8,3 +8,28 @@ const guessRemaining = document.querySelector(".remaining");
 const guessTotal = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
+//console.log(guessedLetter, guessButton, guessInput, wordInProgress, guessRemaining, guessTotal, message, playAgainButton);
+
+const word = "magnolia"; // start word of the game
+
+// // Functions // // 
+// Placeholder function (circles)
+const placeholder = function (word) {
+    const placeholderResults = [];
+    for (const letter of word) {
+      console.log(letter);
+      placeholderResults.push("●");
+    }
+    wordInProgress.innerText = placeholderResults.join("");
+  };
+  
+  placeholder(word);
+  
+  // // Event Listeners //
+  // Guess! button event listener
+  guessButton.addEventListener("click", function (e) {
+    e.preventDefault(); //clear
+    const capture = input.value;
+    console.log(capture);
+    input.value = "";
+  });
