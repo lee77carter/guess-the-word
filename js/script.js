1d7822c11a7bb02fd10c51c967922761a697bdc3
lee_c <gitleelee@gmail.com>
2023-11-20 12:03:28 -0800

    Added global variables.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// // Global Variables - pointing to HTML elements // //
+const guessedLetter = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const guessInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const guessRemaining = document.querySelector(".remaining");
+const guessTotal = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
